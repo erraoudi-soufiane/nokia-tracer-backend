9586e069a8e2a88ce15d87078ddf24b7b62804e6
erraoudi-soufiane <erraoudisoufiane5@gmail.com>
2023-09-26 19:43:41 +0100

    first commit

---
+++ src/main/java/nokia/internship/packettracer/PacketTracerApplication.java
@@ -0,0 +1,13 @@
+package nokia.internship.packettracer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PacketTracerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PacketTracerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/nokia/internship/packettracer/PacketTracerApplicationTests.java
@@ -0,0 +1,13 @@
+package nokia.internship.packettracer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PacketTracerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
